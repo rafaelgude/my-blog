c89d4ca5e5a18ade030793c4296bc984e247876c
Rafael Gude <gudemvl@hotmail.com>
2023-07-09 10:47:49 -0300

    updating versions

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '1ead0d9090b0e7bfb37ac5d6119eafe3bbed7042', queries });
-export default client;
-  
